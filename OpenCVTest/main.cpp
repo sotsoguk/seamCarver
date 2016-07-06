1256def8385e83097e3e02fa948c7d3f93754af1
sotsoguk <sotsoguk@gmail.com>
2016-07-06 16:32:42 +0200

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Bert Riffelmacher on 06.07.16.
+//  Copyright © 2016 Bert Riffelmacher. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
